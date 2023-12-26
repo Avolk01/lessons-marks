255d74e8b397f5c4ab552b8c7111ad88e91dccfb
Avolk01 <volkodavartem01@mail.ru>
2023-12-26 20:32:33 +0300

    feat: init nest js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# lessons-marks
+# develoment branch
+
+1. Move to directory
+   cd .\lessons-marks\
+
+2. Install dependencies
+   npm ci
+
+3. Copy .env file
+   cp .env.example .env.development
+
+4. Start docker container
+   docker compose up -d
+
+5. Start server
+   npm run start:dev
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableCors();
+
+    const docs = new DocumentBuilder()
+        .setTitle('Api documentation')
+        .setDescription('Documentation for lessons-marks api')
+        .setVersion('0.0.1')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, docs);
+
+    SwaggerModule.setup('api/docs', app, document);
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    const configService = app.get(ConfigService);
+
+    const port = configService.get<string>('APP_PORT');
+    const host = configService.get<string>('APP_HOST');
+
+    await app.listen(port);
+
+    Logger.log(`Server is listening on url ${host}:${port}`);
+}
+bootstrap();
